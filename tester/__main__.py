bbead85f8eb363e9b020829ea8925115667f9193
pav.kupriyanov <pav.kupriyanov@vk.team>
2022-08-11 22:16:02 +0300

    Test framework

---
+++ tester/__main__.py
@@ -0,0 +1,66 @@
+from argparse import ArgumentParser
+from importlib import import_module
+from pathlib import Path
+
+parser = ArgumentParser(description='Фреймворк для запуска тестов')
+parser.add_argument('task', metavar='task', help='Имя каталога для запуска тестов')
+parser.add_argument('-c', '--cases', help='Путь к каталогу с тестами относительно module', default='cases')
+parser.add_argument('-i', '--input_ext', help='Разрешение файлов входных данных', default='.in')
+parser.add_argument('-o', '--output_ext', help='Разрешение файлов результатов', default='.out')
+parser.add_argument('-m', '--module', help='Модуль python с кодом решения задачи', default='main')
+parser.add_argument('-f', '--function', help='Имя функции в модуле', default='main')
+parser.add_argument('-d', '--description', help='Путь к файлу с описанием задачи', default='problem.txt')
+
+
+def main(
+        task: str,
+        cases: str = 'cases',
+        input_ext: str = '.in',
+        output_ext: str = '.out',
+        module_name: str = 'main',
+        function_name: str = 'main',
+        description_path: str | None = None
+):
+    module = import_module(f'{task}.{module_name}')
+    solution = getattr(module, function_name)
+
+    path = Path(f'{task}/{cases}')
+
+    if description_path:
+        with open(f'{task}/{description_path}') as fp:
+            print(fp.read())
+
+    for case in path.glob(f'*{input_ext}'):
+        print('-' * 10)
+        case_name = case.name.removesuffix(input_ext)
+        print(case_name)
+
+        with open(next(path.glob(f'{case_name}{output_ext}'))) as fp:
+            expected = fp.readline()
+
+        with open(case) as fp:
+            data = fp.readlines()
+
+        print(f'Входные данные: {" ".join(data)}')
+        print(f'Ожидается: {expected}')
+
+        actual = solution(data)
+        print(f'Получено: {actual}')
+
+        if actual == expected:
+            print('Тест пройден')
+        else:
+            print('ОШИБКА: Тест не пройден')
+
+
+if __name__ == '__main__':
+    args = parser.parse_args()
+    main(
+        args.task,
+        cases=args.cases,
+        input_ext=args.input_ext,
+        output_ext=args.output_ext,
+        module_name=args.module,
+        function_name=args.function,
+        description_path=args.description
+    )
---
+++ tickets/main.py
@@ -0,0 +1,7 @@
+def main(args: list[str]) -> str:
+    result = lucky_tickets(int(args[0]))
+    return str(result)
+
+
+def lucky_tickets(n: int) -> int:
+    return 10
